c5fe49da50b46022d6d4163dc6b55919e1baff6d
ddodgg <60410605+ddodgg@users.noreply.github.com>
2020-01-29 12:13:00 -0600

    Add files via upload

--- MyTreeDemoSucks.java
+++ MyTreeDemoSucks.java
@@ -44,26 +44,74 @@ class MyTreeDemoSucks {
 		third.isEvergreen = false;
 		third.howTall = 20;
 		
+		
+		tree four = new tree(400);
+		
+		four.name = "          Another Oak Tree\n";
+		four.Home2Wildlyfe = true;
+		four.wood = new String[] {"trunk", "branches", "roots"};
+		four.seeds = "round";
+		four.isEvergreen = false;
+		four.howTall = 35;
+		
+		tree five = new tree(530, "Badass Leaf", "Albino", "Rhombicosidodecahedron");
+		
+		five.name = "          Baobab Tree\n";
+		five.Home2Wildlyfe = true;
+		five.wood = new String[] {"trunk", "branches", "roots"};
+		five.seeds = "round";
+		five.isEvergreen = false;
+		five.howTall = 35;
+		
+		tree six = new tree(780, "Sith Lord", "Null", "Vesica Piscis", "          Dead Vlei\n", false, "Darth", false, 42);
+		
+		
 		System.out.print(first.toString()); {
 		System.out.println();
+		System.out.println(" OMG VIAGRA IN ACTION!! GROW tree GROW!\n");
+		first.grow();
+		System.out.print("Tree on viagra! Tree Now " + first.howTall + "ft long."); 
+		System.out.println();
+		five.ChangesDuringFall(first.isEvergreen);
 		System.out.println("\nNext tree: "); 
-		System.out.println("_______________________"); }
+		System.out.println("_______________________"); 
+		
+		}
 		
 		System.out.print(second.toString()); { 
 		System.out.println();
 		System.out.println("\nNext tree: "); 
-		System.out.println("_______________________"); }
+		System.out.println("_______________________"); 
+		}
 		
 		System.out.print(third.toString());{ 
-		System.out.println();		
-		System.out.println("OMG VIAGRA IN ACTION!! GROW tree GROW!\n");
-			third.grow();
-		System.out.print("Tree on viagra! Tree Now " + third.howTall + "ft long."); }
-			System.out.println();
-			third.ChangesDuringFall(third.isEvergreen);
-			
-			
+		System.out.println();	
+		System.out.println("\nNext tree: "); 
+		System.out.println("_______________________"); 
 		}
+		
+		System.out.print(four.toString()); {
+		System.out.println();
+		System.out.println("\nNext tree: "); 
+		System.out.println("_______________________"); 
+		}
+		
+		System.out.print(five.toString()); {
+		System.out.print(five.toString("leaf"));
+		System.out.println();
+		System.out.println("\nNext tree: "); 
+		System.out.println("_______________________");
+		}
+		
+		System.out.print(six.toString());{
+		
+		System.out.print(six.toString(9));
+		System.out.print(six.toString("leaf"));
+		System.out.println();
+			}
+		
+		}		
+		
 	}
 }
 	
