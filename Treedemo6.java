e6577a8b323a5da78a56b905d640592e3768a709
BenjaminThomasJones <60410761+BenjaminThomasJones@users.noreply.github.com>
2020-01-29 12:07:11 -0600

    Add files via upload

--- Treedemo6.java
+++ Treedemo6.java
@@ -1,4 +1,4 @@
-class Treedemo {
+class Treedemo6 {
 	public static void main(String args[]) {
 		
 		tree first = new tree();
@@ -11,50 +11,45 @@ class Treedemo {
 		first.isEvergreen = false;
 		first.howTall = 50;
 		
-		tree second = new tree();
+		tree second = new tree(0);
 		
 		second.name = "dog";
 		second.Home2Wildlyfe = false;
-		second.leaves = new leaf[0];
 		second.wood = new String[]{"bark", "bite", "legg"};
 		second.seeds = "smaller dogs";
 		second.isEvergreen = false;
 		second.howTall = 1;
 		
-		tree third = new tree();
+		tree third = new tree(10, "ever green thing", "nettles", "needles");
 		
 		third.name = "ever green";
 		third.Home2Wildlyfe = true;
-		third.leaves = new leaf[10];
-		third.wood = new String[]{"ever green thing", "nettles", "needles"};
 		third.seeds = "evergreen";
 		third.isEvergreen = true;
 		third.howTall = 35;
 		
+		tree fourth = new tree(1, "jaundice", "corona", "influenza", "bad tree", false, "bad tree seeds",  false, 15);
+		
 		System.out.println(first + "\n");
 		
 		first.grow();
-		
 		first.ChangesDuringFall(first.isEvergreen);
-		
 		System.out.println(first + "\n");
 		
 		first.grow();
-		
 		first.ChangesDuringWinter(first.isEvergreen);
-		
 		System.out.println(first + "\n");
 		
 		first.grow();
-		
 		first.ChangesDuringSpring(first.isEvergreen);
-		
 		System.out.println(first + "\n");
 		
 		first.grow();
-		
 		first.ChangesDuringSummer(first.isEvergreen);
-		
 		System.out.println(first + "\n");
+		
+		System.out.println(third.toString(3 + "\n"));
+		
+		System.out.println(third.toString("EVA" + "\n"));
 	}
 }
