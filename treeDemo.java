509f37200a46bd15cfc2652cab98c736529fe1d8
PhilMcConn <60410681+PhilMcConn@users.noreply.github.com>
2020-01-29 10:15:44 -0600

    Add files via upload

--- treeDemo.java
+++ treeDemo.java
@@ -1,62 +1,72 @@
 class treeDemo
 {
-	public static void main(String[] aa)
+	public static void main(String args[])
 	{
-		tree first = new tree();
-		
-		//System.out.print(first.toString());
-		
-		first.name = "oak";
-		first.Home2Wildlyfe = false;
-		first.leaves = new leaf[3];
-		first.wood = new String[]{"trunk","branches","roots"};
-		first.seeds = "flat";
-		first.isEvergreen = false;
-		first.howTall = 15;
-		
-		System.out.println(first);
-		
-		
-	
-	
-	tree second = new tree();
-		
-		second.name = "pine";
-		second.Home2Wildlyfe = true;
-		second.leaves = new leaf[10];
-		second.wood = new String[]{"trunk","branches","roots"};
-		second.seeds = "flat";
-		second.isEvergreen = false;
-		second.howTall = 50;
-		
-		System.out.println(second);
-	
-	
-	tree third = new tree();
-		
-		third.name = "cactus";
-		third.Home2Wildlyfe = false;
-		third.leaves = new leaf[0];
-		third.wood = new String[]{"trunk","branches","roots"};
-		third.seeds = "";
-		third.isEvergreen = false;
-		third.howTall = 10;
-		
-		System.out.println(third);
-		
-		third.grow();
-				System.out.println(third);
+/*	String name;
+	boolean Home2Wildlyfe;
+	public leaf[] leaves;
+	String[] wood;
+	String seeds;
+	boolean isEvergreen;
+	int howTall;
+*/
 
-		
+tree OakTree = new tree();
+
+OakTree.name = "OakTree";
+OakTree.Home2Wildlyfe = true;
+OakTree.leaves = new leaf[1000];
+OakTree.wood = new String[]{"roots","trunk","branches"};
+OakTree.seeds = "acorns";
+OakTree.isEvergreen = false;
+OakTree.howTall = 100;
+
+tree PecanTree = new tree();
+PecanTree.name = "PecanTree";
+PecanTree.Home2Wildlyfe = true;
+PecanTree.leaves = new leaf[20];
+PecanTree.wood = new String[]{"roots","trunk","branches"};
+PecanTree.seeds = "pecans";
+PecanTree.isEvergreen = false;
+PecanTree.howTall = 144;
+
+tree WalnutTree = new tree();
+WalnutTree.name = "WalnutTree";
+WalnutTree.Home2Wildlyfe = true;
+WalnutTree.leaves = new leaf[200];
+WalnutTree.wood = new String[]{"roots","trunk","branches"};
+WalnutTree.seeds = "walnuts";
+WalnutTree.isEvergreen = true;
+WalnutTree.howTall = 50;
+
+System.out.println(OakTree.toString());
+System.out.println();
+System.out.println(PecanTree.toString());
+System.out.println();
+System.out.println(WalnutTree.toString());
+System.out.println();
+
+OakTree.grow();
+System.out.println(OakTree.toString());
+
+
+OakTree.ChangesDuringWinter(OakTree.isEvergreen);
+System.out.println();
+OakTree.ChangesDuringSpring(OakTree.isEvergreen);
+System.out.println();
+OakTree.ChangesDuringSummer(OakTree.isEvergreen);
+System.out.println();
+OakTree.ChangesDuringFall(OakTree.isEvergreen);
+System.out.println();
+
+
+System.out.println(OakTree);
+
+
 	
-	
-	
-	
-	
-	
-	
+
 	}
 
-
-
 }
+	
+	
